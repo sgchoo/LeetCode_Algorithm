13e8e521e8967cf458305664e75a752e2f47bcae
sgchoo <101913148+sgchoo@users.noreply.github.com>
2024-07-21 18:51:56 +0900

    Time: 50 ms (73.54%), Space: 16.6 MB (32.91%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -3,4 +3,12 @@ class Solution:
         if x < 0:
             return False
 
-        return True if str(x) == str(x)[::-1] else False
+        # return True if str(x) == str(x)[::-1] else False
+        origin = x
+        reverse = 0
+
+        while x > 0:
+            reverse = reverse * 10 + x % 10
+            x = x // 10
+
+        return True if origin == reverse else False
