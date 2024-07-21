b2c111fa41378d6df3592a7c787be40e664746ce
sgchoo <101913148+sgchoo@users.noreply.github.com>
2024-07-21 17:43:35 +0900

    Time: 53 ms (69.69%), Space: 16.7 MB (26.16%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -1,10 +1,17 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        for i in magazine:
-            for j in ransomNote:
-                if i == j:
-                    ransomNote = ransomNote.replace(j, "", 1)
-                    print("current ransomNote is:", ransomNote)
-                    break
+        from collections import Counter
 
-        return True if len(ransomNote) == 0 else False
+        cnt1, cnt2 = Counter(ransomNote), Counter(magazine)
+
+        if cnt1 & cnt2 == cnt1:
+            return True
+        return False
+        # for i in magazine:
+        #     for j in ransomNote:
+        #         if i == j:
+        #             ransomNote = ransomNote.replace(j, "", 1)
+        #             print("current ransomNote is:", ransomNote)
+        #             break
+
+        # return True if len(ransomNote) == 0 else False
