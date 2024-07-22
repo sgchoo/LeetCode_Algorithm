3cec7e286a9d3f11ef299db8b44cff56e7678031
sgchoo <101913148+sgchoo@users.noreply.github.com>
2024-07-22 16:38:01 +0900

    Time: 34 ms (62.56%), Space: 16.3 MB (97.09%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.py
+++ 0290-word-pattern/0290-word-pattern.py
@@ -1,12 +1,18 @@
 class Solution:
     def wordPattern(self, pattern: str, s: str) -> bool:
-        list1, list2 = [], []
-        tempList = s.split(' ')
+        # list1, list2 = [], []
+        # tempList = s.split(' ')
 
-        for i in pattern:
-            list1.append(pattern.find(i))
+        # for i in pattern:
+        #     list1.append(pattern.find(i))
             
-        for j in tempList:
-            list2.append(tempList.index(j))
+        # for j in tempList:
+        #     list2.append(tempList.index(j))
 
-        return True if list1 == list2 else False
+        # return True if list1 == list2 else False
+
+        from itertools import zip_longest
+
+        s = s.split(' ')
+
+        return len(set(pattern)) == len(set(s)) == len(set(zip_longest(pattern, s)))
